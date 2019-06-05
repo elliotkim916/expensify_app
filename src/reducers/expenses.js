0539ff6a1e4dbd72a2f5c41a5d52a1c0f3783969
Elliot Kim <elliotkim916@gmail.com>
2019-06-04 18:01:45 -0700

    Added filter, startDate, and endDate in initialState

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -9,6 +9,9 @@ import {
 
 const initialState = {
   expenses: [],
+  filter: '',
+  startDate: null,
+  endDate: null,
   loading: false,
   error: null
 }
@@ -23,16 +26,14 @@ export function expensesReducer(state=initialState, action) {
       state.expenses.splice(action.index, 1, action.editedExpense)
     );
   } else if (action.type === FILTER_EXPENSE) {
-    // why isnt this working properly?
     return Object.assign({}, state, {
-      expenses: state.expenses.filter(expense => expense.title.toLowerCase().includes(action.title.toLowerCase()))
+      filter: action.title.toLowerCase()
     });
   } else if (action.type === FILTER_BY_DATES) {
-    console.log(state);
     console.log(action);
-    // why isnt this working properly?
     return Object.assign({}, state, {
-      expenses: state.expenses.filter(expense => expense.date >= action.startDate && expense.date <= action.endDate)
+      startDate: action.startDate,
+      endDate: action.endDate
     });
   } else if (action.type === DELETE_EXPENSE) {
     return Object.assign({}, state, {
