59792ba3d32e8e0eb26a0b956a4e52297a9db91d
Elliot Kim <elliotkim916@gmail.com>
2019-06-05 15:49:43 -0700

    Added combineReducers middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {expensesReducer} from './reducers/expenses';
+import {filtersReducer} from './reducers/filter';
 
-const store = createStore(expensesReducer, applyMiddleware(thunk.withExtraArgument(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())));
+const store = createStore(combineReducers({
+    expensesReducer: expensesReducer,
+    filtersReducer: filtersReducer
+  }), 
+  applyMiddleware(thunk.withExtraArgument(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+);
 
 export default store;
