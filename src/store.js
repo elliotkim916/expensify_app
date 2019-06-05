17b512ba3d4b3c3a200f7a0f0af93822b82ae6d0
Elliot Kim <elliotkim916@gmail.com>
2019-06-04 18:02:22 -0700

    Added withExtraArgument for redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {expensesReducer} from './reducers/expenses';
 
-const store = createStore(expensesReducer, applyMiddleware(thunk));
+const store = createStore(expensesReducer, applyMiddleware(thunk.withExtraArgument(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())));
 
 export default store;
