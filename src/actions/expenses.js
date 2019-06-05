288a3903d97fefdfeb0eab1621d070f37eb1d3e7
Elliot Kim <elliotkim916@gmail.com>
2019-06-05 15:48:37 -0700

    Removed all actions that involves filter

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -17,25 +17,6 @@ export const updateExpense = (index, editedExpense) => ({
   editedExpense
 });
 
-export const FILTER_EXPENSE = 'FILTER_EXPENSE';
-export const filterExpense = title => ({
-  type: FILTER_EXPENSE,
-  title
-});
-
-export const FILTER_BY_DATES = 'FILTER_BY_DATES';
-export const filterByDates = (startDate, endDate) => ({
-  type: FILTER_BY_DATES,
-  startDate,
-  endDate
-});
-
-export const EXPENSE_ERROR = 'EXPENSE_ERROR';
-export const expenseError = error => ({
-  type: EXPENSE_ERROR,
-  error
-});
-
 export const addNewExpense = expense => dispatch => {
   const {title, description, amount, date, notes, id} = expense;
   return dispatch(addExpense(expense));
@@ -48,12 +29,4 @@ export const removeExpense = id => dispatch => {
 export const editExpense = (index, editedExpense) => dispatch => {
   const {title, description, amount, date, notes} = editedExpense
   return dispatch(updateExpense(index, editedExpense));
-}
-
-export const filterExpenses = title => dispatch => {
-  return dispatch(filterExpense(title));
-}
-
-export const filterExpensesByDates = (startDate, endDate) => dispatch => {
-  return dispatch(filterByDates(new Date(startDate).getTime(), new Date(endDate).getTime()));
 }
